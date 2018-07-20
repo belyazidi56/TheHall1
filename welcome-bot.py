49706735b260a76a0acddca879cc9bc482e0a53d
Th3legend112 <35711540+Th3legend112@users.noreply.github.com>
2018-07-20 16:34:27 +0100

    Update welcome-bot.py

--- welcome-bot.py
+++ welcome-bot.py
@@ -11,7 +11,12 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('-----')
-newUserMessage = "Welcome " + member.name + " To The Hall :house: Server "
+newUserMessage = """Welcome to The Hall :house: """ + member.name + """ please make sure to read the rules and enjoy your stay
+
+The Hall is a community based server centred around providing a fun and relaxed environment for its users.
+
+We have :
+- Custom Bots , NSFW content , Games , Active Members , Partner and Hypesquad Discord Members , Giveaways , Music , Ranking System  and more... """
 @client.event
 async def on_member_join(member):
     print("Recognised that a member called " + member.name + " joined")
