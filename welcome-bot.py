75706120465b6d3383b591daf9c26f471fa82846
Th3legend112 <35711540+Th3legend112@users.noreply.github.com>
2018-07-22 14:16:55 +0100

    Update welcome-bot.py

--- welcome-bot.py
+++ welcome-bot.py
@@ -14,7 +14,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('-----')
-    await client.change_presence(game=discord.Game(name='The Hall|&help'))
+    await bot.change_presence(game=discord.Game(name='The Hall|&help'))
 
 
 @client.event
