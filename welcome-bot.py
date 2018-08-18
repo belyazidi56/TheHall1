638a3c3df8a52c94800f0fe74853dae83b2a3967
Th3legend112 <35711540+Th3legend112@users.noreply.github.com>
2018-08-18 05:13:00 +0200

    Update welcome-bot.py

--- welcome-bot.py
+++ welcome-bot.py
@@ -79,7 +79,7 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('-----')
-    await bot.change_presence(game=discord.Watching(name='The Hall |&help'), status=discord.Status.dnd)
+    await bot.change_presence(Watching=discord.Game(name='The Hall |&help'), status=discord.Status.dnd)
 
 
 @bot.event
